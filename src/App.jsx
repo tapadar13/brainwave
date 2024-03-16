d80faad3452d33e1a17676bda81f7c7bb7d9d9aa
tapadar13 <monsurismyname@gmail.com>
2024-03-16 15:59:27 +0530

    chore: Add assets and create a resuable Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline uppercase">Hello, World!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline uppercase">Hello, World!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
   );
-}
+};
 
 export default App;
